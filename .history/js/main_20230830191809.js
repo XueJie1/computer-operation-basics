28c53571b75a7113a9c857b04bb0dcc8c42af83d
XueJie1 <3124792285@qq.com>
2023-09-02 21:32:05 +0800

    add more festivals

---
+++ .history/js/main_20230830191809.js
@@ -0,0 +1,19 @@
+
+    $(document).ready(function () {
+        // 为侧边栏链接绑定点击事件
+        $('.sidebar a').on('click', function (e) {
+            e.preventDefault(); // 阻止默认链接行为
+
+            // 获取点击链接的href属性值
+            var target = $(this).attr('href');
+
+            // 隐藏所有的section
+            $('main section').hide();
+
+            // 显示目标section
+            $(target).show();
+        });
+    });
+
+    $("#header").load("header.html");
+        $("#footer").load("footer.html");
---
+++ .history/js/main_20230830210148.js
@@ -0,0 +1,16 @@
+
+    $(document).ready(function () {
+        // 为侧边栏链接绑定点击事件
+        $('.sidebar a').on('click', function (e) {
+            e.preventDefault(); // 阻止默认链接行为
+
+            // 获取点击链接的href属性值
+            var target = $(this).attr('href');
+
+            // 隐藏所有的section
+            $('main section').hide();
+
+            // 显示目标section
+            $(target).show();
+        });
+    });
